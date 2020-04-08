ad48c07c7cf7404f92efbdc6f65da8d7cfec53cf
Madison Randle <madisonrandle25@gmail.com>
2020-04-07 18:43:29 -0600

    Refactor startUp() methods from scripts.js

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,22 +1,56 @@
 import $ from 'jquery';
+import User from './User';
+import UserRepo from './UserRepo';
+import Sleep from './Sleep';
+import Hydration from './Hydration';
+import Activity from './Activity';
+
+import moment from 'moment';
+
+let today, userRepo, user, todaysDate;
 
 const domUpdates = {
-
-  loadPage: (users, sleep, activity, hydration) => {
-    console.log('in domUpdates: ', users, sleep, activity, hydration);
+  loadPage: (users, sleepData, activityData, hydrationData) => {
+    // today = moment().format('YYYY-MM-DD');
+    // sleepData = new Sleep(sleepData);
+    // activityData = new Activity(activityData);
+    // hydrationData = new Hydration(hydrationData);
     domUpdates.findRandomUser(users);
+    userRepo = new UserRepo(users, user);
+    domUpdates.findMostCurrentDate(sleepData);
+
+    // var sortedArray = userRepo.getDataSetForUser(sleepData);
+
+
   },
 
   findRandomUser: (users) => {
-
     for (let i = users.length - 1; i > 0; i--) {
       let n = Math.floor(Math.random() * (i + 1));
       [users[i], users[n]] = [users[n], users[i]];
     }
-    return users[0];
+    user = new User(users[0]);
+
 
   },
 
+  findMostCurrentDate: (dataSet) => {
+    todaysDate = dataSet[dataSet.length - 1].date;
+
+    // userRepo.getDataFromPastWeek(dataSet, todaysDate);
+    // var sortedArray = usersRepo.makeSortedUserArray(user, dataSet);
+
+    // let blah = sortedArray[Math.floor(Math.random() * sortedArray.length + 1)].date
+  },
+
+
+  populateLastWeeksData: () => {
+
+  }
+  // function makeToday(userRepo, currentUserID, hydrationData) {
+  //   var sortedArray = userRepo.makeSortedUserArray(currentUserID, hydrationData);
+  //   return sortedArray[0].date;
+  // }
 
 }
 
