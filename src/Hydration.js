330caeb7e8f781d211c365f432fcb0085363de34
John Watterson <john@breakthrewbrew.com>
2020-04-13 16:24:03 -0600

    Rework Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -9,15 +9,18 @@ class Hydration {
     }, 0) / foundHydrationDataById.length;
   }
   calculateDailyOunces(id, date) {
-    let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
-    return findOuncesByDate.numOunces;
+    let foundOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
+    return foundOuncesByDate.numOunces;
   }
-  calculateFirstWeekOunces(userRepo, id) {
-    return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+  //have this method on userRepo
+  showEntireWeeksFluidConsumption(userRepo, id) {
+    let mostRecentWeekOfHydration = userRepo.getDataFromPastWeek(this.hydrationData);
+    return mostRecentWeekOfHydration.map(currentDay => `${currentDay.date}: ${data.numOunces}`);
+    // return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
   }
-  calculateRandomWeekOunces(date, id, userRepo) {
-    return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
+  // calculateRandomWeekOunces(date, id, userRepo) {
+  //   return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+  // }-------I DO NOT THINK THAT WE WANT A RANDOM WEEK!!!!!
 }
 
 
