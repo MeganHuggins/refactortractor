9186e35ad89c63f9731107b87d007cefcf625ee3
Madison Randle <madisonrandle25@gmail.com>
2020-04-14 20:37:57 -0600

    Add proprty to post data and manipuate sleepData onto the DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,27 +1,16 @@
 import $ from 'jquery';
+import moment from 'moment';
 import User from './User';
 import UserRepo from './UserRepo';
 import Sleep from './Sleep';
 import Hydration from './Hydration';
 import Activity from './Activity';
 
-let userRepo, currentUser, activity, mostCurrentDateInDataSet, hydration;
-
-// let headerText = document.getElementById('headerText');
-// let sidebarName = document.getElementById('sidebarName');
-// let stepGoalCard = document.getElementById('stepGoalCard');
-// let userAddress = document.getElementById('userAddress');
-// let userEmail = document.getElementById('userEmail');
-// let userStridelength = document.getElementById('userStridelength');
-// let friendList = document.getElementById('friendList');
-let mostCurrentWeek = document.querySelectorAll('.historicalWeek');
+let userRepo, currentUser, activity, mostCurrentDateInDataSet, hydration, sleep, formattedDate;
 
 const domUpdates = {
   loadPage: (users, sleepData, activityData, hydrationData) => {
-    // today = moment().format('YYYY-MM-DD');
-    // sleepData = new Sleep(sleepData);
-    // activityData = new Activity(activityData);
-    // hydrationData = new Hydration(hydrationData);
+
     mostCurrentDateInDataSet = domUpdates.findMostCurrentDate(hydrationData);
     currentUser = domUpdates.findRandomUser(users);
     domUpdates.findMostCurrentDate(hydrationData);
@@ -29,17 +18,11 @@ const domUpdates = {
     activity = new Activity(activityData);
     userRepo.getDataFromPastWeek(hydrationData);
     domUpdates.addInfoToSidebar(userRepo);
-    // domUpdates.makeWinnerID(activityData);
     domUpdates.populateLastWeeksData();
     hydration = new Hydration(hydrationData);
-    console.log(hydration);
-    console.log('average daily oz all time', hydration.calculateAverageOunces(currentUser.id));
-    console.log('oz on specified date', hydration.calculateDailyOunces(currentUser.id, "2019/06/17"));
-    console.log('oz consumed daily latest week', hydration.showEntireWeeksFluidConsumption(userRepo, currentUser.id));
-
-    // var sortedArray = userRepo.getDataSetForUser(sleepData);
-
-
+    sleep = new Sleep(sleepData);
+    domUpdates.addSleepInfo();
+    $('#submit-new-activity-button').click((e) => domUpdates.postNewActivityForm(e));
   },
 
   findRandomUser: (users) => {
@@ -50,20 +33,15 @@ const domUpdates = {
     return new User(users[0]);
   },
 
-
   findMostCurrentDate: (dataSet) => {
     let sortedData = dataSet.sort((a, b) => parseInt(a.date) - parseInt(b.date));
     return sortedData[sortedData.length - 1].date;
   },
 
-
   populateLastWeeksData: () => {
-    // console.log($('.historicalWeek'));
     $('.historicalWeek').each((index, value) => {
       $(value).append(`Week of ${mostCurrentDateInDataSet}`);
     });
-      // mostCurrentWeek.forEach(instance => instance.insertAdjacentHTML('afterBegin', `Week of ${mostCurrentDateInDataSet}`));
-      // addInfoToSidebar(currentUser, userRepo);
   },
 
   addInfoToSidebar: (userRepo) => {
@@ -85,10 +63,109 @@ const domUpdates = {
   makeWinnerID: (activityData) => {
     let mostCurrentDateInDataSet = domUpdates.findMostCurrentDate(activityData);
     let weiner = activity.getWinnerId(currentUser, mostCurrentDateInDataSet, userRepo);
-  }
+  },
+
+  addSleepInfo: () => {
+    const formattedDate = moment('2020/01/16', 'YYYY-MM-DD').format('MMM Do YYYY');
+    const currentUserSleepData = userRepo.getDataSetForUser(sleep.sleepData);
+    const hoursSlept = sleep.calculateDailySleep(currentUserSleepData, '2020/01/16');
+    const sleepQualityToday = sleep.calculateDailySleepQuality(currentUserSleepData, '2020/01/16');
+
+    !hoursSlept ?
+      $('#sleepToday').append(`<p>We have no sleep data for you ${formattedDate}</p>`) :
+        $('#sleepToday').append(`
+          <p>You slept <span class="number">${hoursSlept}</span> hours ${formattedDate}.</p>
+        `);
+
+    !sleepQualityToday ?
+      $('#sleepQualityToday').append(`<p>We have no sleep data for you ${formattedDate}</p>`) :
+        $('#sleepQualityToday').append(`<p>Your sleep quality was <span class="number">${sleepQualityToday}</span> out of 5.</p>`);
+
+    $('#sleepThisWeek').append(domUpdates.makeSleepHTML(sleep.calculateWeekSleep(currentUserSleepData, userRepo)));
+  },
+
+  makeSleepHTML: (method) => {
+    formattedDate = moment('2020/01/16', 'YYYY-MM-DD').format('MMM Do YYYY');
+    return method.map(sleepData => `<li class="historical-list-listItem">On ${sleepData} hours</li>`).join('');
+  },
+
+  postNewActivityForm: (e) => {
+    e.preventDefault();
+    const dateInput = $('#calendar').val();
+    const numOuncesInput = parseInt($('#hydration-ounces').val());
+    const numStepsInput = parseInt($('#num-steps').val());
+    const minutesActiveInput = parseInt($('#min-active').val());
+    const flightsOfStairsInput = parseInt($('#flights-stairs').val());
+    const hoursSleepInput = parseInt($('#sleep-hours').val());
+    const sleepQualityInput = parseInt($('#sleep-quality').val());
+    formattedDate = moment(dateInput, 'YYYY-MM-DD').format('YYYY/MM/DD');
+
+    if (dateInput && numOuncesInput) {
+      domUpdates.postNewhydration(dateInput, numOuncesInput);
+    };
+
+    if (dateInput && numStepsInput && minutesActiveInput && flightsOfStairsInput) {
+      domUpdates.postNewSteps(dateInput, numStepsInput, minutesActiveInput, flightsOfStairsInput);
+    };
+
+    if (dateInput && hoursSleepInput && sleepQualityInput) {
+      domUpdates.postNewSleep(dateInput, hoursSleepInput, sleepQualityInput);
+    };
+  },
+
+  postNewhydration: (dateInput, numOuncesInput) => {
+    fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/hydration/hydrationData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "userID": currentUser.id,
+        "date": formattedDate,
+        "numOunces": numOuncesInput
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(`There was an error: ${error}`));
+  },
+
+  postNewSteps: (dateInput, numStepsInput, minutesActiveInput, flightsOfStairsInput) => {
+    fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/activity/activityData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "userID": currentUser.id,
+        "date": formattedDate,
+        "numSteps": numStepsInput,
+        "minutesActive": minutesActiveInput,
+        "flightsOfStairs": flightsOfStairsInput
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(`There was an error: ${error}`));
+  },
+
+  postNewSleep: (dateInput, hoursSleepInput, sleepQualityInput) => {
+    fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          "userID": currentUser.id,
+          "date": formattedDate,
+          "hoursSlept": hoursSleepInput,
+          "sleepQuality": sleepQualityInput
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(`There was an error: ${error}`));
+  },
 }
-
-
-
 
 export default domUpdates;
