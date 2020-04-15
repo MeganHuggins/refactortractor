4ea10e471d88ce68e0e5ebe3991096bbfb499ad1
Megan Huggins <Muggins1265@gmail.com>
2020-04-15 00:49:23 -0600

    Clean up code

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -32,43 +32,11 @@ class UserRepo {
     return weeksWorthOfData;
   };
 
-  // getFriendDataFromPastWeek(friendsActivity) {
-  //   // let friendDataSet = currentUser.friends.map(friend => {
-  //   //   return this.getDataSetForFriends(dataSet, friend)
-  //   // })
-  //   // .sort((a, b) => parseInt(a.date) - parseInt(b.date));
-  //   let indexOfCurrentDate = friendsActivity.indexOf(friendsActivity.find(data => data.date === '2020/01/22'));
-  //   let weeksWorthOfData = friendsActivity.splice((indexOfCurrentDate - 7), 7);
-  //   return weeksWorthOfData;
-  //
-  //
-  //   todaysDate = domUpdates.findMostCurrentDate(friendsActivity);
-  //
-  //   friendsActivity.reduce((acc, friend) =>{
-  //     friend
-  //   }, {})
-  //
-  // };
-
-  // getToday(id, dataSet) { ***Didn't see this called anywhere
-  //   return this.makeSortedUserArray(id, dataSet)[0].date;
-  // };
-
-  //** getFirstWeek is only called once in Hydration.js in the mthod calculateFirstWeekOunces**
-  // getFirstWeek(id, dataSet) {
-  //   return this.makeSortedUserArray(id, dataSet).slice(0, 7);
-  // };
-
-  // getWeekFromDate(date, id, dataSet) {
-  //   let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date)));
-  //   return this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
-  // };
-
   chooseDayDataForAllUsers(activityData, date) {
     return activityData.filter(dataItem => {
       return dataItem.date === date
     });
-  }
+  };
 
   createFriendsDataSetObj(friendsActivity, todaysDate, relevantData) {
     return friendsActivity.reduce((acc, dataItem) => {
@@ -81,7 +49,7 @@ class UserRepo {
       })
       return acc;
     }, {});
-  }
+  };
 
   rankFriendsByDataSets(friendsActivity, todaysDate, relevantData) {
     let friendsDataSets = this.createFriendsDataSetObj(friendsActivity, todaysDate, 'numSteps');
@@ -95,11 +63,11 @@ class UserRepo {
         return acc;
       }, 0) / friendsDataSets[a].length)
     });
-  }
+  };
 
   friendStepsRankings(friendsActivity, todaysDate, releventData) {
     return this.rankFriendsByDataSets(friendsActivity, todaysDate, releventData);
   }
-}
+};
 
 export default UserRepo;
